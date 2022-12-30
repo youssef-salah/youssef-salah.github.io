7ed9c70d2e831e8b11300c9008f02c23c7722198
youssef-salah <77450576+youssef-salah@users.noreply.github.com>
2022-12-30 23:42:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -17,10 +17,11 @@ var carousel = document.querySelector('[data-carousel]');
 var slides = document.getElementsByClassName('carousel-cell');
 var flkty = new Flickity(carousel, options);
 
+
 flkty.on('scroll', function () {
   flkty.slides.forEach(function (slide, i) {
     var image = slides[i];
     var x = (slide.target + flkty.x) * -1/3;
-    image.style.backgroundPosition = x + 'px';
+    image.style.backgroundPositionX =  'center';
   });
 });
